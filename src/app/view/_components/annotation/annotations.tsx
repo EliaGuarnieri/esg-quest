45d1e6ae141835f13567139c64a9170510efc6c8
Elia Guarnieri <elia.guarnieri@gmail.com>
2024-08-13 18:42:58 +0200

    Add page group for better visualization

--- src/app/view/_components/annotation/annotations.tsx
+++ src/app/view/_components/annotation/annotations.tsx
@@ -3,7 +3,7 @@ import "@react-pdf-viewer/highlight/lib/styles/index.css";
 
 import { useIsomorphicLayoutEffect } from "@react-pdf-viewer/core";
 import { highlightPlugin } from "@react-pdf-viewer/highlight";
-import { useContext, useRef, useState } from "react";
+import { Fragment, useContext, useRef, useState } from "react";
 
 import { FileName, PluginsInstance } from "@/app/view/_context";
 import { type Note } from "@/app/view/_types";
@@ -18,6 +18,7 @@ export const Annotations = () => {
   const { fileName } = useContext(FileName);
   const [selectedNote, setSelectedNote] = useState<string | null>(null);
   const noteElementsRef = useRef<Map<string, HTMLElement>>(new Map());
+  const currentPage = useRef<number>(0);
 
   const jumpToNote = (note: Note) => {
     if (noteElementsRef.current.has(note.id)) {
@@ -59,22 +60,34 @@ export const Annotations = () => {
   };
 
   return data.map((note) => {
+    const isSamePage = currentPage.current === note.area.pageIndex;
+    if (!isSamePage) {
+      currentPage.current = note.area.pageIndex;
+    }
     return (
-      <Card
-        key={note.id}
-        className={cn(
-          "mb-2 cursor-pointer border border-accent bg-card shadow-md shadow-accent transition-colors hover:border-slate-50 hover:bg-slate-50",
-          selectedNote === note.id && "bg-accent",
+      <Fragment key={note.id}>
+        {!isSamePage && (
+          <div className="flex justify-center pb-2 pt-4">
+            <span className="rounded-full bg-accent px-4 text-sm text-foreground">
+              On page {currentPage.current}
+            </span>
+          </div>
         )}
-        ref={(ref) =>
-          void noteElementsRef.current.set(note.id, ref as HTMLElement)
-        }
-        onClick={() => handleNoteClick(note)}
-      >
-        <CardContent className="p-2">
-          <div className="z-10 overflow-hidden rounded-sm">{note.text}</div>
-        </CardContent>
-      </Card>
+        <Card
+          className={cn(
+            "mb-2 cursor-pointer border border-accent bg-card text-sm shadow-md shadow-accent transition-colors hover:border-slate-50 hover:bg-slate-50",
+            selectedNote === note.id && "bg-accent",
+          )}
+          ref={(ref) =>
+            void noteElementsRef.current.set(note.id, ref as HTMLElement)
+          }
+          onClick={() => handleNoteClick(note)}
+        >
+          <CardContent className="p-2">
+            <div className="z-10 overflow-hidden rounded-sm">{note.text}</div>
+          </CardContent>
+        </Card>
+      </Fragment>
     );
   });
 };
